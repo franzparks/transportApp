2ec961cc4a50c57e0218bd5d06dc67f5ee73297c
franzparks <franzparks@gmail.com>
2016-05-06 20:01:02 -0700

    feature: add debugger to see service worker processes

--- app/scripts/sw.js
+++ app/scripts/sw.js
@@ -3,6 +3,7 @@
 
   // Load the sw-toolbox library.
   importScripts('/bower_components/sw-toolbox/sw-toolbox.js');
+  global.toolbox.options.debug = true;
   
   // Ensure that our service worker takes control of the page as soon as possible.
   global.addEventListener('install', event => event.waitUntil(global.skipWaiting()));
