971711059939ecdc7da4da088ebc659e0921e0fc
franzparks <franzparks@gmail.com>
2016-05-08 20:15:18 -0700

    chore: add schedules

--- app/sw.js
+++ app/sw.js
@@ -7,32 +7,6 @@
    // Turn on debug logging, visible in the Developer Tools' console.
   global.toolbox.options.debug = true;
 
-//pre install static files for better service offline (this is called during service worker install)
-  /*toolbox.precache([ 
-    '/scripts/xml2json/xml2json.js','/index.html','/bartRoutes.xml',
-    '/getStopsForRoute.xml', '/getNextDeparturesByStopName.xml',
-     'bower_components/bootstrap/dist/css/bootstrap.css',
-     '/styles/main.css', '/styles/styles.css',
-     'bower_components/jquery/dist/jquery.js',
-     'bower_components/angular/angular.js',
-     'bower_components/angular-loading-bar/build/loading-bar.css',
-     'bower_components/bootstrap/dist/js/bootstrap.js',
-     'bower_components/angular-animate/angular-animate.js',
-     'bower_components/angular-aria/angular-aria.js',
-     'bower_components/angular-cookies/angular-cookies.js',
-     'bower_components/angular-messages/angular-messages.js',
-     'bower_components/angular-resource/angular-resource.js',
-     'bower_components/angular-route/angular-route.js',
-     'bower_components/angular-sanitize/angular-sanitize.js',
-     'bower_components/angular-touch/angular-touch.js',
-     'bower_components/angular-loading-bar/build/loading-bar.js',
-     '/scripts/app.js',
-     '/scripts/controllers/main.js',
-     'https://crossorigin.me/https://cdn.rawgit.com/abdmob/x2js/master/xml2json.js',
-     '/sw.js'
-    
-     ]);
-    */ 
  
 //'https://crossorigin.me/http://maps.googleapis.com/maps/api/js?sensor=false',
   
@@ -45,9 +19,9 @@
 
     });
 
-   toolbox.router.get('/getStopsForRoute.xml', global.toolbox.cacheFirst, {
+   toolbox.router.get('/schedules.xml', global.toolbox.cacheFirst, {
        cache: {
-         name: 'bart',
+         name: 'my-cache',
          maxEntries: 50,
          maxAgeSeconds: 86400 // cache for a day
        }
@@ -95,7 +69,7 @@ var myDefaultRequestHandler = function(request, values, options) {
         // fire after the cache has been updated.
         return cache.addAll([
          '/scripts/xml2json/xml2json.js','/index.html','/stations.xml',
-    '/getStopsForRoute.xml', '/getNextDeparturesByStopName.xml',
+    '/schedules.xml',
      'bower_components/bootstrap/dist/css/bootstrap.css',
      '/styles/main.css', '/styles/styles.css',
      'bower_components/jquery/dist/jquery.js',
